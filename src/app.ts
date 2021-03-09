3b225e44491b0332a3082d7167ba4e387a555819
willianlemes <willianlemes94@gmail.com>
2021-03-08 21:11:16 -0300

    Commit inicial

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.json({
+    message: "Hello World",
+  });
+});
+
+export default app;
